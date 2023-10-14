2bef3f690c6af2512b7058e9d5aadf8608b6f77d
Alejandro Salas Jurado <alejandro.salas090603@gmail.com>
2023-10-14 12:29:30 -0500

    Funcion get con parametros

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 app.title = "Mi primera app"
 app.version = "0.0.1"
 
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    },
+    {
+        'id': 2,
+        'title': 'Avatar 2',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    }
+]
 
 @app.get('/',tags=['home'])
 def message():
-    return 'Hola mundo'
+    return HTMLResponse('<h1>Hola mundo</h1>')
+
+@app.get('/movies',tags=['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int):
+    return list(filter(lambda x:x['id']==id,movies))
+
