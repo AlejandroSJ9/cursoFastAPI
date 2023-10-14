3944150c7c1394b9a42223cfdf8c9fbe6448febf
Alejandro Salas Jurado <alejandro.salas090603@gmail.com>
2023-10-14 12:16:03 -0500

    Documentacion /docs

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = "Mi primera app"
+app.version = "0.0.1"
 
-@app.get('/')
+
+@app.get('/',tags=['home'])
 def message():
     return 'Hola mundo'
